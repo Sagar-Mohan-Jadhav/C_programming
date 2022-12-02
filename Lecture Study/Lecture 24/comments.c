e26ba0dc0075e86fa345e345126b934cea2b1dbb
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-12-02 18:47:09 +0100

    comments study

---
+++ Lecture Study/Lecture 24/comments.c
@@ -0,0 +1,12 @@
+/*
+This is a multiline comment.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    // This is single line comment.
+    printf("This program is to check how comments work.");
+    return 0;
+}
