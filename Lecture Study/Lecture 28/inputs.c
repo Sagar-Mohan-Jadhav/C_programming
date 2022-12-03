238d4d1b1d31b05758c6e80ec6b9eb1d93be32ed
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-12-03 23:46:08 +0100

    inputs

---
+++ Lecture Study/Lecture 28/inputs.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    char name[100];
+    double grade;
+    printf("Hello, Enter your age, your name and grade separated by white space: ");
+    scanf("%d %s %lf", &age, name, &grade);
+    printf("Welcome %s, your age is %d and your grade is %lf", name, age, grade);
+}
