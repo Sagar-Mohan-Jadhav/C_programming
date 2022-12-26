9cc2d72042af7bbac00984caf2b7d2ce98b04421
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-12-26 12:53:41 +0100

    Pass by reference study done

---
+++ Challenges/Challenge 104/Pass by reference.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+void square1(int *number)
+{
+    *number = (*number)*(*number);
+    return;
+}
+
+int *square2(int *number)
+{
+    *number = (*number)*(*number);
+    return number;
+}
+int main()
+{
+    int num = 6;
+
+    square1(&num);
+    printf("The result of square1 is %d.\n", num);
+    num = 5;
+    square2(&num);
+    printf("The result of square2 is %d.\n", num);
+
+    return 0;
+}
