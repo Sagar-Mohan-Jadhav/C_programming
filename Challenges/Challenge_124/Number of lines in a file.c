985aa4ec04a270a90ff785af2bb7044f4479e391
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2022-12-29 19:15:24 +0100

    Challenge 124 complete

--- Challenges/Challenge_124/Number of lines in a file.c
+++ Challenges/Challenge_124/Number of lines in a file.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
     FILE *pfile;
-
-    pfile = fopen("text_file.txt", "r");
+    pfile = fopen("TextFile.txt", "r");
+    if (false)
+    {
+        printf("The file could not be opened.\n");
+    }
+    else
+    {
+        int ch, len, count = 0, eof_count = 0;
+        fseek(pfile, 0, SEEK_END);
+        len = ftell(pfile);
+        printf("The length of the text file is %d.\n",len);
+        fseek(pfile, 0, SEEK_SET);
+        while(count <= len)
+        {
+            if ((ch = fgetc(pfile)) == 13)
+            {
+                eof_count++;
+            }
+            count++;
+        }
+        printf("There are %d lines in the text file.\n", eof_count);
+    }
+    fclose(pfile);
+    pfile = NULL;
     return 0;
 }
