e1bb5aafc3072f261c18b1db701d5b3e7c168b1d
Sagar-Mohan-Jadhav <69190284+Sagar-Mohan-Jadhav@users.noreply.github.com>
2023-01-02 18:02:57 +0100

    player strategy counter
    Now computer determines potential user strategy and counters it

--- Project_Tic_Tac_Toe/Tic_Tak_Toe.c
+++ Project_Tic_Tac_Toe/Tic_Tak_Toe.c
@@ -65,7 +65,7 @@ void user_turn(int *play_board0, int *play_board1, int *play_board2)
     return;
 }
 
-void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_move_row, int *best_move_column)
+void definite_risk(int *play_board0, int *play_board1, int *play_board2, int *best_move_row, int *best_move_column)
 {
     bool risk = false;
     int risk_row = 3, risk_count = 0, risk_column = 3, risk_diag = 3;
@@ -77,8 +77,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
         }
         if (risk_count >= 2)
         {
-            risk_row = 0;
-            risk = true;
+            if ((*(play_board0)+ *(play_board0 + 1)+ *(play_board0 + 2)) != 4)
+            {
+                risk_row = 0;
+                risk = true;
+            }
             risk_count = 0;
             break;
         }
@@ -92,8 +95,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
         }
         if (risk_count >= 2)
         {
-            risk_row = 1;
-            risk = true;
+            if ((*(play_board1)+ *(play_board1 + 1)+ *(play_board1 + 2)) != 4)
+            {
+                risk_row = 1;
+                risk = true;
+            }
             risk_count = 0;
             break;
         }
@@ -107,8 +113,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
         }
         if (risk_count >= 2)
         {
-            risk_row = 2;
-            risk = true;
+            if ((*(play_board2)+ *(play_board2 + 1)+ *(play_board2 + 2)) != 4)
+            {
+                risk_row = 2;
+                risk = true;
+            }
             risk_count = 0;
             break;
         }
@@ -131,8 +140,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
         }
         if (risk_count >= 2)
         {
-            risk_column = count;
-            risk = true;
+            if ((*(play_board0 + count)+ *(play_board1 + count)+ *(play_board2 + count)) != 4)
+            {
+                risk_column = count;
+                risk = true;
+            }
             risk_count = 0;
             break;
         }
@@ -152,8 +164,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
     }
     if (risk_count >= 2)
     {
-        risk_diag = 0;
-        risk = true;
+        if ((*(play_board0)+ *(play_board1 + 1)+ *(play_board2 + 2)) != 4)
+        {
+            risk_diag = 0;
+            risk = true;
+        }
         risk_count = 0;
     }
     risk_count = 0;
@@ -171,8 +186,11 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
     }
     if (risk_count >= 2)
     {
-        risk_diag = 1;
-        risk = true;
+        if ((*(play_board2)+ *(play_board1 + 1)+ *(play_board0 + 2)) != 4)
+        {
+            risk_diag = 1;
+            risk = true;
+        }
         risk_count = 0;
     }
     if (risk)
@@ -307,6 +325,188 @@ void best_move(int *play_board0, int *play_board1, int *play_board2, int *best_m
     return;
 }
 
+void potential_risk(int *play_board0, int *play_board1, int *play_board2, int *best_move_row, int *best_move_column)
+{
+    int risk_count = 0;
+    if (*(play_board0)== 0)
+    {
+        if ((*(play_board0 + 1) + *(play_board0 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 0) + *(play_board2 + 0)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 1)+ *(play_board2 + 2))== 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 0;
+            *(best_move_column) = 0;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board0 + 1)== 0)
+    {
+        if ((*(play_board0) + *(play_board0 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 1) + *(play_board2 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 0;
+            *(best_move_column) = 1;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board0 + 2)== 0)
+    {
+        if ((*(play_board0) + *(play_board0 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 2) + *(play_board2 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 1)+ *(play_board2 + 0))== 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 0;
+            *(best_move_column) = 2;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board1)== 0)
+    {
+        if ((*(play_board1 + 1) + *(play_board1 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board0) + *(play_board2 + 0)) == 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 1;
+            *(best_move_column) = 0;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board1 + 1)== 0)
+    {
+        if ((*(play_board0) + *(play_board2 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1) + *(play_board1 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board0 + 1)+ *(play_board2 + 1))== 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board2)+ *(play_board0 + 2))== 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 1;
+            *(best_move_column) = 1;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board1 + 2)== 0)
+    {
+        if ((*(play_board1) + *(play_board1 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board0 + 2) + *(play_board2 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 1;
+            *(best_move_column) = 2;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board2)== 0)
+    {
+        if ((*(play_board0) + *(play_board1)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board2 + 1) + *(play_board2 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 1)+ *(play_board0 + 2))== 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 2;
+            *(best_move_column) = 0;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board2 + 1)== 0)
+    {
+        if ((*(play_board2) + *(play_board2 + 2)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 1) + *(play_board0 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 2;
+            *(best_move_column) = 1;
+        }
+    }
+    risk_count = 0;
+    if (*(play_board2 + 2)== 0)
+    {
+        if ((*(play_board0) + *(play_board1 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board2) + *(play_board2 + 1)) == 1)
+        {
+            risk_count++;
+        }
+        if ((*(play_board1 + 2)+ *(play_board0 + 2))== 1)
+        {
+            risk_count++;
+        }
+        if (risk_count>= 2)
+        {
+            *(best_move_row) = 2;
+            *(best_move_column) = 2;
+        }
+    }
+    return;
+}
+
 void computer_turn(int *play_board0, int *play_board1, int *play_board2, int *best_move_row, int *best_move_column)
 {
     printf("Computers play.\n");
@@ -338,7 +538,8 @@ int main()
         {
             user_turn(play_board[0], play_board[1], play_board[2]);
             disp_board(play_board[0], play_board[1], play_board[2]);
-            best_move(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+            potential_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+            definite_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
             computer_turn(play_board[0], play_board[1], play_board[2],&best_move_row, &best_move_column);
             disp_board(play_board[0], play_board[1], play_board[2]);
         }
@@ -349,13 +550,15 @@ int main()
     {
         for (int count = 0; count <= 3; count++)
         {
-            best_move(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+            potential_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+            definite_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
             computer_turn(play_board[0], play_board[1], play_board[2],&best_move_row, &best_move_column);
             disp_board(play_board[0], play_board[1], play_board[2]);
             user_turn(play_board[0], play_board[1], play_board[2]);
             disp_board(play_board[0], play_board[1], play_board[2]);
         }
-        best_move(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+        potential_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
+        definite_risk(play_board[0], play_board[1], play_board[2], &best_move_row, &best_move_column);
         computer_turn(play_board[0], play_board[1], play_board[2],&best_move_row, &best_move_column);
         disp_board(play_board[0], play_board[1], play_board[2]);
     }
